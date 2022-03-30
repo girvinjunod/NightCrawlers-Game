0aed583701f5ebdee8bf181d4659158151de81e7
girvinjunod <girvinjunod@gmail.com>
2022-03-31 00:13:32 +0700

    refactor: change screen resolution for main meny

--- Survival Shooter/Assets/MainMenu.cs
+++ Survival Shooter/Assets/MainMenu.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    // void Awake()
+    // {
+    //     Resolution[] resolutions = Screen.resolutions;
+
+    //     // Print the resolutions
+    //     foreach (var res in resolutions)
+    //     {
+    //         Debug.Log(res.width + "x" + res.height + " : " + res.refreshRate);
+    //     }
+    // }
     public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
+
 
     }
 }
