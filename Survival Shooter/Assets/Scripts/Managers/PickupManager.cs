d8bae1b5365a1f1bdff0111646bb750369cc2a06
Zenovore <agustinus.alexander17@gmail.com>
2022-04-07 01:20:53 +0700

    feat/orb-implementation

---
+++ Survival Shooter/Assets/Scripts/Managers/PickupManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class PickupManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject powerPickup;
+    public GameObject healthPickup;
+    public GameObject speedPickup;
+    public float spawnTime = 30f;
+    public Transform[] spawnPoints;
+    public static List<Vector3> usedSpawnPoints = new List<Vector3>();
+
+
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        // int numOfTries = 10;
+        // for (int i = 0; i < numOfTries; i++)
+        // {
+        if (!usedSpawnPoints.Contains(spawnPoints[spawnPointIndex].position)){                                  
+            usedSpawnPoints.Add(spawnPoints[spawnPointIndex].position);
+            // RANDOM ALGORITHM
+            int random = Random.Range(0, 3);
+            if (random == 0){
+                Instantiate(powerPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            } else if (random == 1){
+                Instantiate(speedPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            } else if (random == 2){
+                Instantiate(healthPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);            
+            }
+        }
+        // }
+    }
+}
---
+++ Survival Shooter/Assets/Scripts/Pickup/Pickup.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public enum PickupType {Power, Speed, Health};
+
+    public PickupType pickupType = PickupType.Power;
+    GameObject player;
+    bool playerInRange;
+    protected PlayerHealth playerHealth;
+    protected PlayerShooting playerShooting;
+    protected PlayerMovement playerMovement;
+
+    public float delay = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        playerShooting = player.GetComponentInChildren <PlayerShooting> ();
+        playerMovement = player.GetComponentInChildren <PlayerMovement> ();
+        StartCoroutine(SelfDestruct());
+        // enemyHealth = GetComponent<EnemyHealth>();
+        // anim = GetComponent <Animator> ();
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player && other.isTrigger == false)
+        {
+            playerInRange = true;
+        }       
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange)
+        {
+            PickupManager.usedSpawnPoints.Remove(transform.position);            
+            Debug.Log(transform.position);
+        switch (pickupType) {
+          case PickupType.Power:
+            playerShooting.powerOrb();
+            break;
+            
+          case PickupType.Speed:
+            playerMovement.speedOrb();
+            break;
+            
+          case PickupType.Health:
+            playerHealth.healthOrb();
+            break;
+        }
+            Destroy(gameObject,0f);
+        } 
+    } 
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(delay);
+        PickupManager.usedSpawnPoints.Remove(transform.position); 
+        Destroy(gameObject);
+    }
+}
