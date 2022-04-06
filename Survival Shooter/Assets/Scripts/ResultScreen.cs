98bc24284001dbbe22d4165d8ba85589a81ff114
girvinjunod <girvinjunod@gmail.com>
2022-04-06 19:18:03 +0700

    refactor: add more text to result screen to display player results

--- Survival Shooter/Assets/Scripts/ResultScreen.cs
+++ Survival Shooter/Assets/Scripts/ResultScreen.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using TMPro;
 public class ResultScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject EndlessScoreboard;
 
     public GameObject WaveScoreboard;
+
+    public TextMeshProUGUI text1;
+
+    public TextMeshProUGUI text2;
+
+    // public GameObject textBox;
     void Awake()
     {
         // PlayerPrefs.DeleteKey("EndlessScoreboard");
         //enable the scoreboard
-        // StateHolder.GameMode = "WaveMode";
+        // StateHolder.GameMode = "ZenMode";
 
         if (StateHolder.GameMode == "WaveMode")
         {
+            text1.text = "Score: " + StateHolder.playerScore;
+            text2.text = "Wave: " + StateHolder.playerWave;
+            text1.enabled = true;
+            text2.enabled = true;
+            // textBox.SetActive(true);
             WaveScoreboard.SetActive(true);
         }
         else if (StateHolder.GameMode == "ZenMode")
         {
+            text2.text = "Time: " + Utils.getTimeFromSeconds(StateHolder.playerTime);
+            text2.enabled = true;
+            // textBox.SetActive(true);
             EndlessScoreboard.SetActive(true);
         }
     }
