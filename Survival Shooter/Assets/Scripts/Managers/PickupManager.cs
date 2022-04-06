2c912fe7848ee907e693177c33745235b3076944
Zenovore <agustinus.alexander17@gmail.com>
2022-04-07 01:27:16 +0700

    Add orb implementation when reach max stats

--- Survival Shooter/Assets/Scripts/Managers/PickupManager.cs
+++ Survival Shooter/Assets/Scripts/Managers/PickupManager.cs
@@ -30,12 +30,18 @@ public class PickupManager : MonoBehaviour
         if (!usedSpawnPoints.Contains(spawnPoints[spawnPointIndex].position)){                                  
             usedSpawnPoints.Add(spawnPoints[spawnPointIndex].position);
             // RANDOM ALGORITHM
-            int random = Random.Range(0, 3);
-            if (random == 0){
+            int random = Random.Range(1, 3);
+            if (random == 0 && PowerManager.power < 200){
                 Instantiate(powerPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-            } else if (random == 1){
+            } else {
+              random = Random.Range(1,2);
+            }
+            if (random == 1 && SpeedManager.speed < 10){
                 Instantiate(speedPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-            } else if (random == 2){
+            } else {
+              random = 2;
+            }
+            if (random == 2 ){
                 Instantiate(healthPickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);            
             }
         }
